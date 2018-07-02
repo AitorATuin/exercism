d9f2e8bbd6c05b46e08830fba4076a4024d9598e
ATuinDev <1757663+AitorATuin@users.noreply.github.com>
2018-07-02 22:24:43 +0200

    Simplify elixir/secret-handshake

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -1,15 +1,18 @@
 defmodule Op do
+  use Bitwise
   defstruct val: nil, f: nil
+
+  defp _run(xs, %Op{val: v, f: f}, code) when (code &&& v) === v, do: f.(xs)
+  defp _run(xs, %Op{val: _, f: _}, _), do: xs
+  def run(code), do: fn op, xs -> _run(xs, op, code) end
 end
 
 defmodule SecretHandshake do
-  use Bitwise
-
   def secretcode,
     do: [
-      %Op{val: 1, f: fn xs -> ["wink" | xs] end},
-      %Op{val: 2, f: fn xs -> ["double blink" | xs] end},
-      %Op{val: 4, f: fn xs -> ["close your eyes" | xs] end},
+      %Op{val: 1, f: &["wink" | &1]},
+      %Op{val: 2, f: &["double blink" | &1]},
+      %Op{val: 4, f: &["close your eyes" | &1]},
       %Op{val: 8, f: &["jump" | &1]},
       %Op{val: 16, f: &Enum.reverse/1}
     ]
@@ -17,26 +20,9 @@ defmodule SecretHandshake do
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
-
-  If the following bits are set, include the corresponding action in your list
-  of commands, in order from lowest to highest.
-
-  1 = wink
-  10 = double blink
-  100 = close your eyes
-  1000 = jump
-
-  10000 = Reverse the order of the operations in the secret handshake
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
-    ops = Enum.map(secretcode, fn op ->
-      case op do
-        # %Op{val: v, f: f} -> f
-        %Op{val: v, f: f} when (code &&& v) == v -> f
-        _ -> & &1
-      end
-    end)
-    ops |> Enum.reduce([], fn f, xs -> f.(xs) end) |> Enum.reverse
+    secretcode() |> Enum.reduce([], Op.run(code)) |> Enum.reverse()
   end
 end
