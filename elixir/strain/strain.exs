ef836ff57b024378ff5015dfbc42105f028de346
ATuinDev <1757663+AitorATuin@users.noreply.github.com>
2018-07-03 21:02:14 +0200

    Add solution elixir/strain

---
+++ elixir/strain/strain.exs
@@ -0,0 +1,32 @@
+defmodule Strain do
+
+  # TODO: Implement a macro to use fun as a guard
+  defp filter([], _fun), do: [] 
+  defp filter([x|xs], fun) do
+    if fun.(x) do [x | filter(xs, fun)]
+    else filter(xs, fun)
+    end
+  end
+
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns true.
+
+  Do not use `Enum.filter`.
+  """
+  @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def keep(list, fun) do
+    filter(list, fun)
+  end
+
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns false.
+
+  Do not use `Enum.reject`.
+  """
+  @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def discard(list, fun) do
+    filter(list, & not fun.(&1))
+  end
+end
