3cb7b4d96c3a8f469efe86eec801b28d5f8dde6d
ATuinDev <1757663+AitorATuin@users.noreply.github.com>
2018-11-22 22:01:31 +0100

    Modify racket/difference-of-squares

--- racket/difference-of-squares/difference-of-squares.rkt
+++ racket/difference-of-squares/difference-of-squares.rkt
@@ -1,16 +1,17 @@
-#lang racket
+#lang racket/base
 
 (provide sum-of-squares square-of-sum difference)
 
 (define (sum-of-n pre-f post-f n)
-  (pre-f (for/sum ([i (in-naturals 1)]
-                  #:final (= i n))
-          (post-f i))))
+  (pre-f (for/sum ([i (in-range 1 (add1 n))])
+           (post-f i))))
 
-(define (expt-2 x) (expt x 2))
+(define (square x) (expt x 2))
 
-(define (square-of-sum n) (sum-of-n expt-2 identity n))
+(define (id x) x)
 
-(define (sum-of-squares n) (sum-of-n identity expt-2 n))
+(define (square-of-sum n) (sum-of-n square id n))
+
+(define (sum-of-squares n) (sum-of-n id square n))
 
 (define (difference n) (- (square-of-sum n) (sum-of-squares n)))
